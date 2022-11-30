fc009e29891c6f491e1c97898745aeaa0bc143c5
Pawel Krupinski <pawel.krupinski@gmail.com>
2022-11-29 17:34:33 -0800

    Refactoring.

--- app/db/UpdateEvents.scala
+++ app/db/UpdateEvents.scala
@@ -8,6 +8,7 @@ import play.api.inject.ApplicationLifecycle
 import play.api.{Configuration, Environment, Mode}
 
 import java.io.File
+import scala.collection.parallel.CollectionConverters.seqIsParallelizable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
@@ -34,6 +35,11 @@ trait Common {
       case OrganizerType.EventBrite => EventBrite.organizersEvents(organizer.url)
     }
   }
+
+  def organizerOfEvent(eventUrl: String): Organizer = {
+    val urlList = List(eventUrl)
+    (TicketTailor.fetchOrganizers(urlList) ++ EventBrite.fetchOrganizers(urlList)).head
+  }
 }
 
 object UpdateEvents extends App with Repositories with Common {
@@ -47,17 +53,19 @@ object UpdateEvents extends App with Repositories with Common {
 //    .foreach(eventRepository.)
 
   println(events.mkString("\n"))
-  events.foreach(eventRepository.upsert)
+  events.filter(_.organizerUrl == null).par.map(event => event.copy(organizerUrl = organizerOfEvent(event.url).url)).foreach(eventRepository.replace)
+}
+
+object UpdateExistingEvents extends App with Repositories with Common {
+  private lazy val events = Await.result(eventRepository.all.toFuture(), Duration.Inf)
+  events.filter(_.organizerUrl == "").par
+    .map(event => event.copy(organizerUrl = organizerOfEvent(event.url).url))
+    .foreach(eventRepository.replace)
 }
 
 object InsertOrganizer extends App with Repositories with Common {
 
-  def makeOrganizer(url: String): Organizer = {
-    val urlList = List(url)
-    (TicketTailor.fetchOrganizers(urlList) ++ EventBrite.fetchOrganizers(urlList)).head
-  }
-
-  val organizer = makeOrganizer("https://www.eventbrite.co.uk/o/hulya-erbeyli-coaching-39859153553")
+  val organizer = organizerOfEvent("https://www.eventbrite.co.uk/o/hulya-erbeyli-coaching-39859153553")
   organizerRepository.upsert(organizer)
 
   eventsOf(organizer).foreach(eventRepository.upsert)
